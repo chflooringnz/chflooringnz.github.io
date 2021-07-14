6684f50d6ab5e639cfa91988da5449cf1d32e715
chflooringnz <chflooringnz@gmail.com>
2021-07-14 12:47:22 +1200

    Added review system

---
+++ js/review-table.js
@@ -0,0 +1,33 @@
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let data = JSON.parse(this.responseText).feed.entry;
+
+    let i;
+    for (i = 0; i < data.length; i++) {
+      let time = data[i]["gsx$time"]["$t"];
+      let name = data[i]["gsx$name"]["$t"];
+      let review = data[i]["gsx$review"]["$t"];
+
+      document.getElementById("demo").innerHTML +=
+	    "<tr>" +
+		"<td>" +
+		time +
+		"</td>" +
+		"<td>" +
+		name + 
+		"</td>" +
+		"<td>" +
+		review +
+		"</td>" +
+		"</tr>";
+    }
+  }
+};
+
+xmlhttp.open(
+  "GET",
+  "https://spreadsheets.google.com/feeds/list/1FpcFJtoyGMJ4RRbrAXqFsSqQ78CxYRewasIzgzW8j8U/od6/public/values?alt=json",
+  true
+);
+xmlhttp.send();
