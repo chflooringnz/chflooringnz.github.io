bc2fc958a7c04fe5a03d349bf88ba33aed0bcabd
chflooringnz <84168712+chflooringnz@users.noreply.github.com>
2021-10-09 17:26:28 +1300

    Update review fetch table

--- js/review-table.js
+++ js/review-table.js
@@ -2,7 +2,7 @@ function update_table() {
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      let data = JSON.parse(this.responseText).feed.entry;
+      let data = $.csv.toArrays(this.responseText);
   
       document.getElementById("demo").innerHTML = "";
 
@@ -28,15 +28,9 @@ function update_table() {
     }
   };
   
-  fetch('https://docs.google.com/spreadsheets/d/1FpcFJtoyGMJ4RRbrAXqFsSqQ78CxYRewasIzgzW8j8U/gviz/tq?tqx=out:json')
-    .then(res => res.text())
-    .then(text => {
-        const json = JSON.parse(text.substr(47).slice(0, -2))
-    });
-
-  xmlhttp.open(
+    xmlhttp.open(
     "GET",
-    json,
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1XUtiaDSvawsF1SH_9I-TVC4F04y5xu4VMG8tZv_KbNmjG69H4KRRsSHWxw7U9wfxV72vqqD88fJF/pub?gid=0&single=true&output=csv",
     true
   );
   xmlhttp.send();
